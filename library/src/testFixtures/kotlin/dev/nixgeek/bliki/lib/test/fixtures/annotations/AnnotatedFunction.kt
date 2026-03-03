4013436f264789bb24df755674b6714a2705b49a
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-03 03:53:45 -0800

    add fixtures

--- library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/annotations/AnnotatedFunction.kt
+++ library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/annotations/AnnotatedFunction.kt
@@ -1,6 +1,14 @@
 package dev.nixgeek.bliki.lib.test.fixtures.annotations
 
 import dev.nixgeek.bliki.lib.annotations.FeatureFlag
+import dev.nixgeek.bliki.lib.annotations.NotATestContainerBean
+import org.springframework.context.annotation.Bean
+
+class NotATestContainerBeanAnnotatedFunction {
+    @Bean
+    @NotATestContainerBean
+    fun annotatedFoo(): String = "Hello, world!"
+}
 
 class FeatureFlagAnnotatedFunction {
     @FeatureFlag(name = "TestOnFunction", enabled = false)
