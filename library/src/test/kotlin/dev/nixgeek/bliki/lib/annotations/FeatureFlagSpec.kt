511df3b9a4adcf4da164e61e638efbc38f68f7c4
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-03 03:51:57 -0800

    sneak in additional feature flag tests

--- library/src/test/kotlin/dev/nixgeek/bliki/lib/annotations/FeatureFlagSpec.kt
+++ library/src/test/kotlin/dev/nixgeek/bliki/lib/annotations/FeatureFlagSpec.kt
@@ -2,7 +2,10 @@ package dev.nixgeek.bliki.lib.annotations
 
 import dev.nixgeek.bliki.lib.test.fixtures.annotations.FeatureFlagAnnotatedClass
 import dev.nixgeek.bliki.lib.test.fixtures.annotations.FeatureFlagAnnotatedFunction
+import dev.nixgeek.bliki.lib.test.fixtures.annotations.NotATestContainerBeanAnnotatedClass
+import dev.nixgeek.bliki.lib.test.fixtures.annotations.NotATestContainerBeanAnnotatedFunction
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import kotlin.reflect.full.findAnnotation
@@ -46,5 +49,21 @@ class FeatureFlagSpec : FunSpec({
         featureFlagAnnotation.shouldNotBeNull()
         featureFlagAnnotation.name shouldBe "TestOnFunction"
         featureFlagAnnotation.enabled shouldBe false
+    }
+
+    test("should not find feature flag annotation on class") {
+        val featureFlagAnnotation = NotATestContainerBeanAnnotatedClass::class
+            .findAnnotation<FeatureFlag>()
+
+        featureFlagAnnotation.shouldBeNull()
+    }
+
+    test("should not find feature flag annotation on function") {
+        val featureFlagAnnotation = NotATestContainerBeanAnnotatedFunction::class
+            .members
+            .first { it.name == "annotatedFoo" }
+            .findAnnotation<FeatureFlag>()
+
+        featureFlagAnnotation.shouldBeNull()
     }
 })
