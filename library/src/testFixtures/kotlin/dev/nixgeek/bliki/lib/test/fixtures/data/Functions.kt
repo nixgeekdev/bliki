dbebe3a63bbdf9f8b0529ffb97fd9e6c05baf60f
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-06 11:50:19 -0800

    add ulid test fixtures

---
+++ library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/data/Functions.kt
@@ -0,0 +1,37 @@
+package dev.nixgeek.bliki.lib.test.fixtures.data
+
+import org.jetbrains.exposed.v1.core.Transaction
+import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.insert
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+
+fun withUlidTable(db: Database, statement: Transaction.(tester: TestUlidTable) -> Unit) =
+    TestUlidTable.let { tester ->
+        transaction(db) {
+            SchemaUtils.create(tester)
+            tester.insert { it[ulid] = "01KJYBXX3SJD958F8B39EEPJAA" }
+            statement(tester)
+            SchemaUtils.drop(tester)
+        }
+    }
+
+fun withUlidEntity(db: Database, statement: Transaction.(entity: TestUlidEntity) -> Unit) =
+    TestUlidTable.let { table ->
+        transaction(db) {
+            SchemaUtils.create(table)
+            val entity = TestUlidEntity.new("01KJYC701ZGKRH4WDK30J7TWTM") { ulid = "01KJYC701ZGKRH4WDK30J7TWTM" }
+            statement(entity)
+            SchemaUtils.drop(table)
+        }
+    }
+
+fun withTestUlidTableWithCustomSerializer(db: Database, statement: Transaction.(tester: TestUlidTableWithCustomSerializer) -> Unit) =
+    TestUlidTableWithCustomSerializer.let { tester ->
+        transaction(db) {
+            SchemaUtils.create(tester)
+            tester.insert { it[id] = "01KJYCH1HSSF9ARYRDJZY882GH" }
+            statement(tester)
+            SchemaUtils.drop(tester)
+        }
+    }
---
+++ library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/data/TestUlidTable.kt
@@ -0,0 +1,12 @@
+package dev.nixgeek.bliki.lib.test.fixtures.data
+
+import dev.nixgeek.bliki.lib.data.ulid.ULIDTable
+import dev.nixgeek.bliki.lib.data.ulid.ulid
+
+object TestUlidTable : ULIDTable<String>(
+    name = "test_ulid_table",
+    serializer = TestULIDSerializer,
+    ulidGenerator = { "01KJYB1H3PVXCKRERQPWPKM9JG" },
+) {
+    val ulid = ulid<String>("ulid", TestULIDSerializer)
+}
