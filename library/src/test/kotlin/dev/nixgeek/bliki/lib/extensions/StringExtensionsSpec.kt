21854fec5e37679e220f73954e844dad473ffb63
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-07 19:17:17 -0800

    add tons of tests + fix some

--- library/src/test/kotlin/dev/nixgeek/bliki/lib/extensions/StringExtensionsSpec.kt
+++ library/src/test/kotlin/dev/nixgeek/bliki/lib/extensions/StringExtensionsSpec.kt
@@ -1,42 +1,41 @@
 package dev.nixgeek.bliki.lib.extensions
 
-import dev.nixgeek.bliki.lib.test.fixtures.extensions.*
 import dev.nixgeek.bliki.lib.test.fixtures.extensions.wrappers.StringBalancedWrapper
 import dev.nixgeek.bliki.lib.test.fixtures.extensions.wrappers.StringBase64Wrapper
 import dev.nixgeek.bliki.lib.test.fixtures.extensions.wrappers.StringHexWrapper
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 
 class StringExtensionsSpec : FunSpec({
     context("should decode base64 string correctly") {
         withData(
-            StringBase64Wrapper(TEST_STRING_BASE64_001, TEST_BYTES_BASE64_001.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_002, TEST_BYTES_BASE64_002.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_003, TEST_BYTES_BASE64_003.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_004, TEST_BYTES_BASE64_004.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_005, TEST_BYTES_BASE64_005.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_006, TEST_BYTES_BASE64_006.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_007, TEST_BYTES_BASE64_007.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_008, TEST_BYTES_BASE64_008.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_009, TEST_BYTES_BASE64_009.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_010, TEST_BYTES_BASE64_010.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_011, TEST_BYTES_BASE64_011.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_012, TEST_BYTES_BASE64_012.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_013, TEST_BYTES_BASE64_013.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_014, TEST_BYTES_BASE64_014.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_015, TEST_BYTES_BASE64_015.toByteArray()),
-            StringBase64Wrapper(TEST_STRING_BASE64_016, TEST_BYTES_BASE64_016.toByteArray()),
+            StringBase64Wrapper("", "".toByteArray()),
+            StringBase64Wrapper("Zg==", "f".toByteArray()),
+            StringBase64Wrapper("Zm9v", "foo".toByteArray()),
+            StringBase64Wrapper("Zm9vYg==", "foob".toByteArray()),
+            StringBase64Wrapper("Zm9vYmE=", "fooba".toByteArray()),
+            StringBase64Wrapper("Zm9vYmFy", "foobar".toByteArray()),
+            StringBase64Wrapper("SGVsbG8gd29ybGQ=", "Hello world".toByteArray()),
+            StringBase64Wrapper("aGVsbG8=", "hello".toByteArray()),
+            StringBase64Wrapper("MTIzNDU=", "12345".toByteArray()),
+            StringBase64Wrapper("c3VwZXIgc2VjcmV0", "super secret".toByteArray()),
+            StringBase64Wrapper("w6k=", "é".toByteArray()),
+            StringBase64Wrapper("44GT44KT44Gr44Gh44Gv", "こんにちは".toByteArray()),
+            StringBase64Wrapper("8J+YgA==", "😀".toByteArray()),
         ) { (base64String, expected) ->
             base64String.fromBase64() shouldBe expected
         }
 
         withData(
-            TEST_BAD_BASE64_001,
-            TEST_BAD_BASE64_002,
-            TEST_BAD_BASE64_003,
-            TEST_BAD_BASE64_004,
+            "%",
+            "%%%",
+            "not-base64",
+            "SGVsbG8=*",
         ) { invalidBase64String ->
             shouldThrow<IllegalArgumentException> {
                 invalidBase64String.fromBase64()
@@ -46,25 +45,29 @@ class StringExtensionsSpec : FunSpec({
 
     context("should decode hex string correctly") {
         withData(
-            StringHexWrapper(TEST_STRING_HEX_001, byteArrayOf(0x00)),
-            StringHexWrapper(TEST_STRING_HEX_002, byteArrayOf(0x0f)),
-            StringHexWrapper(TEST_STRING_HEX_003, byteArrayOf(0x0f)),
-            StringHexWrapper(TEST_STRING_HEX_004, byteArrayOf(0xff.toByte())),
-            StringHexWrapper(TEST_STRING_HEX_005, "Hello".toByteArray()),
-            StringHexWrapper(TEST_STRING_HEX_006, "hello".toByteArray()),
-            StringHexWrapper(TEST_STRING_HEX_007, "Hello world".toByteArray()),
-            StringHexWrapper(TEST_STRING_HEX_008, byteArrayOf()),
-            StringHexWrapper(TEST_STRING_HEX_009, byteArrayOf(0x0f)),
+            StringHexWrapper("00", byteArrayOf(0x00)),
+            StringHexWrapper("0f", byteArrayOf(0x0f)),
+            StringHexWrapper("0f", byteArrayOf(0x0f)),
+            StringHexWrapper("ff", byteArrayOf(0xff.toByte())),
+            StringHexWrapper("48656c6c6f", "Hello".toByteArray()),
+            StringHexWrapper("68656c6c6f", "hello".toByteArray()),
+            StringHexWrapper("48656c6c6f20776f726c64", "Hello world".toByteArray()),
+            StringHexWrapper("", byteArrayOf()),
+            StringHexWrapper("0f", byteArrayOf(0x0f)),
+            StringHexWrapper(
+                "deadbeef",
+                byteArrayOf(0xde.toByte(), 0xad.toByte(), 0xbe.toByte(), 0xef.toByte()),
+            ),
         ) { (hexString, expected) ->
             hexString.fromHex() shouldBe expected
         }
 
         withData(
-            TEST_STRING_BAD_HEX_001,
-            TEST_STRING_BAD_HEX_002,
-            TEST_STRING_BAD_HEX_003,
-            TEST_STRING_BAD_HEX_004,
-            TEST_STRING_BAD_HEX_005,
+            "GG",
+            "0xFF",
+            "hello",
+            "12 34",
+            "zz",
         ) { invalidHexString ->
             shouldThrow<NumberFormatException> {
                 invalidHexString.fromHex()
@@ -74,38 +77,212 @@ class StringExtensionsSpec : FunSpec({
 
     context("should identify balanced strings correctly") {
         withData(
-            StringBalancedWrapper(TEST_STRING_BALANCED_001, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_002, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_003, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_004, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_005, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_006, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_007, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_008, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_009, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_010, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_011, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_012, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_013, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_014, true),
-            StringBalancedWrapper(TEST_STRING_BALANCED_015, true),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_001, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_002, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_003, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_004, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_005, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_006, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_007, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_008, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_009, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_010, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_011, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_012, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_013, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_014, false),
-            StringBalancedWrapper(TEST_STRING_UNBALANCED_015, false),
+            StringBalancedWrapper("", true),
+            StringBalancedWrapper("()", true),
+            StringBalancedWrapper("[]", true),
+            StringBalancedWrapper("{}", true),
+            StringBalancedWrapper("<>", true),
+            StringBalancedWrapper("no brackets at all", true),
+            StringBalancedWrapper("a[b{c(d)e}f]g", true),
+            StringBalancedWrapper("<{[(||)]}>", true),
+            StringBalancedWrapper("hello(world)", true),
+            StringBalancedWrapper("[|text|]", true),
+            StringBalancedWrapper("(]", false),
+            StringBalancedWrapper("([)]", false),
+            StringBalancedWrapper("(()", false),
+            StringBalancedWrapper("())", false),
+            StringBalancedWrapper("{[(])}", false),
+            StringBalancedWrapper("<(>", false),
+            StringBalancedWrapper("|||", false),
+            StringBalancedWrapper("([|]|", false),
+            StringBalancedWrapper("(|)", false),
+            StringBalancedWrapper("<<<<{{{{[[[[(|)[|text|](|)]]]]}}}}>>>>(((((((((((((()", false),
         ) { (value, expected) ->
             value.isBalanced() shouldBe expected
+        }
+    }
+
+    context("should handle nullIfBlank correctly") {
+        "".nullIfBlank().shouldBeNull()
+        "   ".nullIfBlank().shouldBeNull()
+        "hello".nullIfBlank() shouldBe "hello"
+        " hello ".nullIfBlank() shouldBe " hello "
+    }
+
+    context("should normalize whitespace correctly") {
+        withData(
+            "" to "",
+            "   " to "",
+            "hello" to "hello",
+            " hello   world " to "hello world",
+            "hello\t\tworld" to "hello world",
+            "hello\nworld" to "hello world",
+            "hello \n\t world" to "hello world",
+        ) { (value, expected) ->
+            value.normalizedWhitespace() shouldBe expected
+        }
+    }
+
+    context("should truncate at word boundary correctly") {
+        "short text".truncateAtWordBoundary(20) shouldBe "short text"
+        "hello brave new world".truncateAtWordBoundary(13) shouldBe "hello brave…"
+        "supercalifragilistic".truncateAtWordBoundary(10) shouldBe "supercali…"
+        "  hello   world  ".truncateAtWordBoundary(8) shouldBe "hello…"
+        "hello world".truncateAtWordBoundary(1) shouldBe "…"
+
+        shouldThrow<IllegalArgumentException> {
+            "hello".truncateAtWordBoundary(-1)
+        }
+
+        shouldThrow<IllegalArgumentException> {
+            "hello".truncateAtWordBoundary(2, "...")
+        }
+    }
+
+    context("should create excerpts correctly") {
+        val markdown =
+            """
+            # Hello World
+
+            This is a **post** with [a link](https://example.com) and [[Internal Page]].
+            """.trimIndent()
+
+        markdown.toExcerpt(24) shouldBe "Hello World This is a…"
+        "Plain text".toExcerpt(200) shouldBe "Plain text"
+    }
+
+    context("should strip markdown correctly") {
+        val markdown =
+            """
+            # Title
+
+            > quoted text
+
+            This is **bold** and _italic_ and `inlineCode`.
+
+            - first item
+            - second item
+
+            [JetBrains](https://jetbrains.com)
+            ![Logo](https://example.com/logo.png)
+            [[Internal Page]]
+
+            <p>html content</p>
+
+            ```kotlin
+            val ignored = true
+            ```
+            """.trimIndent()
+
+        markdown.stripMarkdown() shouldBe
+            "Title quoted text This is bold and italic and inlineCode. first item second item JetBrains Internal Page html content"
+    }
+
+    context("should extract markdown links correctly") {
+        val markdown =
+            """
+            [One](https://example.com/one)
+            [Two](/two)
+            [Three](mailto:test@example.com)
+            [One Again](https://example.com/one)
+            """.trimIndent()
+
+        markdown.extractMarkdownLinks() shouldContainExactlyInAnyOrder
+            setOf(
+                "https://example.com/one",
+                "/two",
+                "mailto:test@example.com",
+            )
+
+        "no links here".extractMarkdownLinks() shouldBe emptySet()
+    }
+
+    context("should extract wiki links correctly") {
+        val markdown = "See [[Page One]] and [[Page Two]] and [[Page One]]"
+
+        markdown.extractWikiLinks() shouldContainExactlyInAnyOrder
+            setOf(
+                "Page One",
+                "Page Two",
+            )
+
+        "no wiki links".extractWikiLinks() shouldBe emptySet()
+    }
+
+    context("should detect wiki links correctly") {
+        "See [[Page One]]".containsWikiLinks() shouldBe true
+        "No internal links here".containsWikiLinks() shouldBe false
+    }
+
+    context("should replace wiki links correctly") {
+        val value = "See [[Page One]] and [[Page Two]]"
+
+        value.replaceWikiLinks { target ->
+            """<a href="/wiki/${target.lowercase().replace(" ", "-")}">$target</a>"""
+        } shouldBe
+            """See <a href="/wiki/page-one">Page One</a> and <a href="/wiki/page-two">Page Two</a>"""
+    }
+
+    context("should parse comma separated tags correctly") {
+        "kotlin, spring, webflux".parseCommaSeparatedTags() shouldContainExactly
+            listOf("kotlin", "spring", "webflux")
+
+        " kotlin, spring  boot , , webflux , kotlin ".parseCommaSeparatedTags() shouldContainExactly
+            listOf("kotlin", "spring boot", "webflux")
+
+        "".parseCommaSeparatedTags() shouldBe emptyList()
+        " , , ".parseCommaSeparatedTags() shouldBe emptyList()
+    }
+
+    context("should validate tag names correctly") {
+        "kotlin".isValidTagName() shouldBe true
+        "spring boot".isValidTagName() shouldBe true
+        "build-tools".isValidTagName() shouldBe true
+        "kotlin_2".isValidTagName() shouldBe true
+
+        "".isValidTagName() shouldBe false
+        "   ".isValidTagName() shouldBe false
+        "!!!".isValidTagName() shouldBe false
+        "spring,boot".isValidTagName() shouldBe false
+        "tag/with/slash".isValidTagName() shouldBe false
+    }
+
+    context("should count words correctly") {
+        "".wordCount() shouldBe 0
+        "hello".wordCount() shouldBe 1
+        "hello world from kotlin".wordCount() shouldBe 4
+        "**hello** [world](https://example.com) [[Internal Page]]".wordCount() shouldBe 4
+    }
+
+    context("should estimate reading time correctly") {
+        "".estimatedReadingTimeMinutes() shouldBe 0
+        "one two three".estimatedReadingTimeMinutes(200) shouldBe 1
+        List(200) { "word" }.joinToString(" ").estimatedReadingTimeMinutes(200) shouldBe 1
+        List(201) { "word" }.joinToString(" ").estimatedReadingTimeMinutes(200) shouldBe 2
+
+        shouldThrow<IllegalArgumentException> {
+            "hello".estimatedReadingTimeMinutes(0)
+        }
+    }
+
+    context("should count lines safely") {
+        "".lineCountSafe() shouldBe 0
+        "hello".lineCountSafe() shouldBe 1
+        "hello\nworld".lineCountSafe() shouldBe 2
+        "hello\r\nworld\r\nagain".lineCountSafe() shouldBe 3
+    }
+
+    context("should quote strings correctly") {
+        "hello".quoted() shouldBe "\"hello\""
+        "".quoted() shouldBe "\"\""
+    }
+
+    context("should ellipsize correctly") {
+        "short text".ellipsize(20) shouldBe "short text"
+        "hello brave new world".ellipsize(13) shouldBe "hello brave…"
+
+        shouldThrow<IllegalArgumentException> {
+            "hello".ellipsize(-1)
         }
     }
 })
---
+++ library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/extensions/Constants.kt
@@ -0,0 +1,3 @@
+package dev.nixgeek.bliki.lib.test.fixtures.extensions
+
+const val TEST_STRING = "test"
