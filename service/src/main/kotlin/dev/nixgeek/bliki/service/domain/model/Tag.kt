204cbbb7fc160277c3b3b6e4c3632273838d664d
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-09 04:23:17 -0700

    fix tag scheme + add parent id

--- service/src/main/kotlin/dev/nixgeek/bliki/service/domain/model/Tag.kt
+++ service/src/main/kotlin/dev/nixgeek/bliki/service/domain/model/Tag.kt
@@ -5,19 +5,21 @@ import kotlin.time.Instant
 
 data class Tag(
     val id: ULID? = null,
+    val parentId: ULID? = null,
     val term: String,
     val slug: String,
     val label: String? = null,
-    val scheme: String? = null,
+    val scheme: TagScheme? = null,
     val createdAt: Instant?,
     val updatedAt: Instant?,
 ) {
     fun toPublicTag(): PublicTag =
         PublicTag(
             id = id!!,
+            parentId = parentId,
             term = term,
             slug = slug,
             label = label,
-            scheme = scheme,
+            scheme = scheme?.type,
         )
 }
