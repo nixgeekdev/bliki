c3b63ccf71a4dbea4bc71eea16e1f8ed10d9833f
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-09 19:19:03 -0700

    moved ulid test fixtures to another package

--- library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/data/ulid/TestUlidEntity.kt
+++ library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/data/ulid/TestUlidEntity.kt
@@ -1,4 +1,4 @@
-package dev.nixgeek.bliki.lib.test.fixtures.data
+package dev.nixgeek.bliki.lib.test.fixtures.data.ulid
 
 import dev.nixgeek.bliki.lib.data.ulid.ULIDEntity
 import dev.nixgeek.bliki.lib.data.ulid.ULIDEntityClass
--- library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/data/ulid/TestUlidTableWithCustomSerializer.kt
+++ library/src/testFixtures/kotlin/dev/nixgeek/bliki/lib/test/fixtures/data/ulid/TestUlidTableWithCustomSerializer.kt
@@ -1,4 +1,4 @@
-package dev.nixgeek.bliki.lib.test.fixtures.data
+package dev.nixgeek.bliki.lib.test.fixtures.data.ulid
 
 import dev.nixgeek.bliki.lib.data.ulid.ULIDSerializer
 import dev.nixgeek.bliki.lib.data.ulid.ULIDTable
