93bfe77c672ea913240efd7b2a286aa0f7dfa1fe
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-14 13:04:22 -0700

    removed helper functions

--- service/src/test/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/repository/ExposedAppGeneratorRepositorySpec.kt
+++ service/src/test/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/repository/ExposedAppGeneratorRepositorySpec.kt
@@ -2,22 +2,23 @@ package dev.nixgeek.bliki.service.frameworks.data.exposed.repository
 
 import dev.nixgeek.bliki.lib.data.DatabaseProvider
 import dev.nixgeek.bliki.lib.data.DatabaseTarget
-import dev.nixgeek.bliki.lib.data.ulid.toULID
 import dev.nixgeek.bliki.lib.test.fixtures.containers.installSharedSpecDatabase
 import dev.nixgeek.bliki.lib.test.fixtures.shared.Constants
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.BlikiTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.GeneratorTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.IdentityTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.ProfileTable
+import dev.nixgeek.bliki.service.test.fixtures.data.insertBliki
+import dev.nixgeek.bliki.service.test.fixtures.data.insertGenerator
+import dev.nixgeek.bliki.service.test.fixtures.data.insertIdentity
+import dev.nixgeek.bliki.service.test.fixtures.data.insertProfile
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
-import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.deleteAll
-import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.springframework.test.context.ActiveProfiles
 import ulid.ULID
@@ -148,65 +149,6 @@ class ExposedAppGeneratorRepositorySpec : FunSpec() {
 
                 result.shouldBeNull()
             }
-        }
-    }
-}
-
-private fun insertGenerator(
-    db: Database,
-    id: ULID,
-    name: String,
-    version: String,
-    uri: String? = null,
-) {
-    transaction(db) {
-        GeneratorTable.insert {
-            it[GeneratorTable.id] = id.toString()
-            it[GeneratorTable.name] = name
-            it[GeneratorTable.version] = version
-            it[GeneratorTable.uri] = uri
-        }
-    }
-}
-
-private fun insertIdentity(db: Database): ULID =
-    transaction(db) {
-        val identityId = ULID.randomULID()
-        IdentityTable.insert {
-            it[IdentityTable.id] = identityId.toString()
-            it[IdentityTable.email] = "test@example.com"
-            it[IdentityTable.passwordHash] = $$"$argon2id$v=19$m=65536,t=2,p=1$Zm9vYmFy$Zm9vYmFy"
-        }
-        identityId.toULID()
-    }
-
-private fun insertProfile(db: Database, identityId: ULID): ULID =
-    transaction(db) {
-        val profileId = ULID.randomULID()
-        ProfileTable.insert {
-            it[ProfileTable.id] = profileId.toString()
-            it[ProfileTable.identityId] = identityId.toString()
-            it[ProfileTable.fullName] = "Test User"
-            it[ProfileTable.affiliation] = "Test Organization"
-        }
-        profileId.toULID()
-    }
-
-private fun insertBliki(
-    db: Database,
-    blikiId: ULID,
-    generatorId: ULID,
-    authorId: ULID,
-) {
-    transaction(db) {
-        BlikiTable.insert {
-            it[BlikiTable.id] = blikiId.toString()
-            it[BlikiTable.title] = "My Bliki"
-            it[BlikiTable.rights] = "Copyright 2026 nixgeek.dev"
-            it[BlikiTable.baseUri] = "https://example.test/bliki"
-            it[BlikiTable.lang] = "en/US"
-            it[BlikiTable.authorId] = authorId.toString()
-            it[BlikiTable.generatorId] = generatorId.toString()
         }
     }
 }
