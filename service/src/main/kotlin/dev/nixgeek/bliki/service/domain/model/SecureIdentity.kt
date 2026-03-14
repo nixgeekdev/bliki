bc9eacb931d2e23fd3d7b2ab9f1ca30001a295e1
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-14 14:30:01 -0700

    various helper methods for model conversion

--- service/src/main/kotlin/dev/nixgeek/bliki/service/domain/model/SecureIdentity.kt
+++ service/src/main/kotlin/dev/nixgeek/bliki/service/domain/model/SecureIdentity.kt
@@ -1,11 +1,9 @@
 package dev.nixgeek.bliki.service.domain.model
 
 import ulid.ULID
-import kotlin.time.Instant
 
 data class SecureIdentity(
     val id: ULID,
     val email: String,
-    val createdAt: Instant?,
-    val updatedAt: Instant?,
+    val passwordHash: String,
 )
--- service/src/main/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/entity/IdentityRoleEntity.kt
+++ service/src/main/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/entity/IdentityRoleEntity.kt
@@ -1,5 +1,9 @@
 package dev.nixgeek.bliki.service.frameworks.data.exposed.entity
 
+import dev.nixgeek.bliki.lib.data.ulid.toULID
+import dev.nixgeek.bliki.service.domain.model.Identity
+import dev.nixgeek.bliki.service.domain.model.IdentityRole
+import dev.nixgeek.bliki.service.domain.model.Role
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.IdentityRoleTable
 import org.jetbrains.exposed.v1.core.dao.id.CompositeID
 import org.jetbrains.exposed.v1.core.dao.id.EntityID
@@ -14,4 +18,22 @@ class IdentityRoleEntity(id: EntityID<CompositeID>) : CompositeEntity(id) {
 
     val identityId: String = identity.id.value
     val roleId: String = role.id.value
+
+    internal fun toRoleModel(): Role =
+        Role(
+            id = roleId.toULID(),
+            role = IdentityRole.valueOf(role.role),
+            label = role.label,
+            createdAt = role.createdAt,
+            updatedAt = role.updatedAt,
+        )
+
+    internal fun toIdentityModel(): Identity =
+        Identity(
+            id = identityId.toULID(),
+            email = identity.email,
+            passwordHash = identity.passwordHash,
+            createdAt = role.createdAt,
+            updatedAt = role.updatedAt,
+        )
 }
