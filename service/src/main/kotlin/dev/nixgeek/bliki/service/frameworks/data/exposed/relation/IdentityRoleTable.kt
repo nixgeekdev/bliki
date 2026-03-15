1e8f6630b1ddab00f0b79611d10b5cbc56e333ab
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-15 02:01:48 -0700

    fix identity role table reference types

--- service/src/main/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/relation/IdentityRoleTable.kt
+++ service/src/main/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/relation/IdentityRoleTable.kt
@@ -3,8 +3,8 @@ package dev.nixgeek.bliki.service.frameworks.data.exposed.relation
 import org.jetbrains.exposed.v1.core.dao.id.CompositeIdTable
 
 object IdentityRoleTable : CompositeIdTable("identity_roles") {
-    val identityId = reference("identity_id", IdentityTable.id).entityId()
-    val roleId = reference("role_id", RoleTable.id).entityId()
+    val identityId = reference("identity_id", IdentityTable.id)
+    val roleId = reference("role_id", RoleTable.id)
 
     override val primaryKey = PrimaryKey(identityId, roleId)
 }
