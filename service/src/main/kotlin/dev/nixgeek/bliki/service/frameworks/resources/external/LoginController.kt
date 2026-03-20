41b3cc7a0058e7164d73aafe5a259a4ab7ffd8f5
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-20 12:38:16 -0700

    add logging

--- service/src/main/kotlin/dev/nixgeek/bliki/service/frameworks/resources/external/LoginController.kt
+++ service/src/main/kotlin/dev/nixgeek/bliki/service/frameworks/resources/external/LoginController.kt
@@ -6,6 +6,7 @@ import dev.nixgeek.bliki.service.frameworks.resources.config.properties.JwtPrope
 import dev.nixgeek.bliki.service.frameworks.service.AuthenticationService
 import dev.nixgeek.bliki.service.frameworks.service.JwtService
 import dev.nixgeek.bliki.service.shared.resources.Routes
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.validation.Valid
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,24 +15,28 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
+private val log = KotlinLogging.logger(LoginController::class.java.canonicalName)
+
 @RestController
-@RequestMapping(Routes.LOGIN_BASE)
+@RequestMapping(
+    value = [Routes.LOGIN_BASE],
+    consumes = [MediaType.APPLICATION_JSON_VALUE],
+    produces = [MediaType.APPLICATION_JSON_VALUE],
+)
 class LoginController(
     private val authenticationService: AuthenticationService,
     private val jwtService: JwtService,
     private val jwtProperties: JwtProperties,
 ) {
-    @PostMapping(
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE],
-    )
+    @PostMapping
     fun login(
-        @Valid
-        @RequestBody
-        request: LoginRequest,
+        @Valid @RequestBody request: LoginRequest,
     ): Mono<LoginResponse> =
         authenticationService
             .authenticate(request.email, request.password)
+            .doOnSubscribe { log.debug { "Login request received for email: ${request.email}" } }
+            .doOnSuccess { log.debug { "Login successful for email: ${request.email}" } }
+            .doOnError { log.error(it) { "Login failed for email: ${request.email}: ${it.message}" } }
             .flatMap { identity ->
                 jwtService
                     .createAccessToken(identity)
