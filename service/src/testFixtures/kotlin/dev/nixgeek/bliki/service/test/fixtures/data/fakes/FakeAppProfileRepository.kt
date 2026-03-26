8a7f68ec1aaf41a1a3e616202adc92a25e1c951c
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-26 08:44:17 -0700

    add fake test fixtures

---
+++ service/src/testFixtures/kotlin/dev/nixgeek/bliki/service/test/fixtures/data/fakes/FakeAppProfileRepository.kt
@@ -0,0 +1,28 @@
+package dev.nixgeek.bliki.service.test.fixtures.data.fakes
+
+import dev.nixgeek.bliki.lib.data.DatabaseProvider
+import dev.nixgeek.bliki.lib.data.ulid.toULID
+import dev.nixgeek.bliki.lib.test.fixtures.data.fakes.AbstractFakeTestRepository
+import dev.nixgeek.bliki.service.domain.model.Profile
+import dev.nixgeek.bliki.service.domain.repository.AppProfileRepository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import ulid.ULID
+
+class FakeAppProfileRepository(
+    override val dbProvider: DatabaseProvider,
+) : AppProfileRepository, AbstractFakeTestRepository<ULID, Profile>() {
+    override fun fetchAll(): Flux<Profile> = blockingFlux { cache.values }
+
+    override fun fetchById(id: ULID): Mono<Profile> = blockingMono { cache[id] }
+
+    override fun fetchByIdentityId(identityId: ULID): Mono<Profile> =
+        blockingMono { cache.values.singleOrNull { it.identityId == identityId } }
+
+    override fun create(record: Profile): Profile {
+        val key = record.id ?: ULID.randomULID().toULID()
+        val created = record.copy(id = record.id ?: key)
+        cache[key] = created
+        return created
+    }
+}
