362e6645da86d8a482c37517a0f09044b3c346d2
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-29 07:15:32 -0700

    add to revision model extension

--- service/src/main/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/repository/ToModelExtensions.kt
+++ service/src/main/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/repository/ToModelExtensions.kt
@@ -6,11 +6,13 @@ import dev.nixgeek.bliki.service.domain.model.Generator
 import dev.nixgeek.bliki.service.domain.model.Identity
 import dev.nixgeek.bliki.service.domain.model.IdentityRole
 import dev.nixgeek.bliki.service.domain.model.Profile
+import dev.nixgeek.bliki.service.domain.model.Revision
 import dev.nixgeek.bliki.service.domain.model.Role
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.BlikiTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.GeneratorTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.IdentityTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.ProfileTable
+import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.RevisionTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.RoleTable
 import org.jetbrains.exposed.v1.core.ResultRow
 
@@ -63,6 +65,19 @@ internal fun ResultRow.toProfileModel(): Profile =
             affiliation = row[ProfileTable.affiliation],
             createdAt = row[ProfileTable.createdAt],
             updatedAt = row[ProfileTable.updatedAt],
+        )
+    }
+
+internal fun ResultRow.toRevisionModel(): Revision =
+    let { row ->
+        Revision(
+            id = row[RevisionTable.id].value.toULID(),
+            entryId = row[RevisionTable.entryId].value.toULID(),
+            authorId = row[RevisionTable.authorId].value.toULID(),
+            diff = row[RevisionTable.diff],
+            summary = row[RevisionTable.summary],
+            event = row[RevisionTable.event],
+            createdAt = row[RevisionTable.createdAt],
         )
     }
 
