123028194b5a6659e9736dd69e692e944e3b7d13
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-30 07:42:17 -0700

    get centent type from mime type

--- service/src/main/kotlin/dev/nixgeek/bliki/service/domain/model/EntryContentType.kt
+++ service/src/main/kotlin/dev/nixgeek/bliki/service/domain/model/EntryContentType.kt
@@ -3,4 +3,10 @@ package dev.nixgeek.bliki.service.domain.model
 enum class EntryContentType(val mimeType: String) {
     TEXT("text/plain"),
     MARKDOWN("text/markdown"),
+    ;
+
+    companion object {
+        fun fromMimeType(mimeType: String): EntryContentType? =
+            values().find { it.mimeType == mimeType }
+    }
 }
