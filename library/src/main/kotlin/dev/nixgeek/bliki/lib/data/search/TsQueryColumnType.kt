06d2dcc4cea11324f34cfb0138bc6cb5eb9e44f6
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-30 12:56:28 -0700

    refactor column types for FTS

--- library/src/main/kotlin/dev/nixgeek/bliki/lib/data/search/TsQueryColumnType.kt
+++ library/src/main/kotlin/dev/nixgeek/bliki/lib/data/search/TsQueryColumnType.kt
@@ -1,9 +1,29 @@
 package dev.nixgeek.bliki.lib.data.search
 
 import org.jetbrains.exposed.v1.core.ColumnType
+import org.postgresql.util.PGobject
 
 class TsQueryColumnType : ColumnType<String>() {
-    override fun sqlType(): String = "TSQUERY"
+    companion object {
+        private const val TS_QUERY_SQL_TYPE = "TSQUERY"
+    }
+
+    override fun sqlType(): String = TS_QUERY_SQL_TYPE
 
     override fun valueFromDB(value: Any): String? = value.toString()
+
+    override fun valueToDB(value: String?): Any? =
+        PGobject()
+            .apply {
+                type = TS_QUERY_SQL_TYPE
+                this.value = value
+            }.value
+
+    override fun notNullValueToDB(value: String): Any =
+        PGobject().apply {
+            type = TS_QUERY_SQL_TYPE
+            this.value = value
+        }
+
+    override fun nonNullValueToString(value: String): String = "'$value'"
 }
--- library/src/main/kotlin/dev/nixgeek/bliki/lib/data/search/TsVectorColumnType.kt
+++ library/src/main/kotlin/dev/nixgeek/bliki/lib/data/search/TsVectorColumnType.kt
@@ -6,7 +6,11 @@ import org.jetbrains.exposed.v1.core.vendors.currentDialect
 import org.postgresql.util.PGobject
 
 class TsVectorColumnType : ColumnType<String>() {
-    override fun sqlType(): String = "TSVECTOR"
+    companion object {
+        private const val TS_VECTOR_SQL_TYPE = "TSVECTOR"
+    }
+
+    override fun sqlType(): String = TS_VECTOR_SQL_TYPE
 
     override fun valueFromDB(value: Any): String? =
         when (value) {
@@ -30,4 +34,20 @@ class TsVectorColumnType : ColumnType<String>() {
                 error("Unexpected value for TSVECTOR: $value of ${value::class.qualifiedName}")
             }
         }
+
+    override fun valueToDB(value: String?): Any? =
+        PGobject()
+            .apply {
+                type = TS_VECTOR_SQL_TYPE
+                this.value = value
+            }.value
+
+    override fun notNullValueToDB(value: String): Any =
+        PGobject()
+            .apply {
+                type = TS_VECTOR_SQL_TYPE
+                this.value = value
+            }
+
+    override fun nonNullValueToString(value: String): String = "'$value'"
 }
