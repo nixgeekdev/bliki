637d424da8584455c62657fff4d6687019606877
Jonas Gorauskas <jgorauskas@gmail.com>
2026-03-30 13:22:45 -0700

    fix delete test

--- service/src/test/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/repository/ExposedAdminRevisionRepositorySpec.kt
+++ service/src/test/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/repository/ExposedAdminRevisionRepositorySpec.kt
@@ -4,11 +4,14 @@ import dev.nixgeek.bliki.lib.data.DatabaseProvider
 import dev.nixgeek.bliki.lib.data.DatabaseTarget
 import dev.nixgeek.bliki.lib.test.fixtures.containers.installSharedSpecDatabase
 import dev.nixgeek.bliki.service.domain.model.EntryEvent
+import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.BlikiTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.EntryTable
+import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.GeneratorTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.IdentityTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.ProfileTable
 import dev.nixgeek.bliki.service.frameworks.data.exposed.relation.RevisionTable
 import dev.nixgeek.bliki.service.test.fixtures.data.insertRevision
+import dev.nixgeek.bliki.service.test.fixtures.data.setupRevisionSimpleFixtures
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -26,8 +29,11 @@ class ExposedAdminRevisionRepositorySpec : FunSpec() {
         installSharedSpecDatabase(
             arrayOf(
                 RevisionTable,
+                EntryTable,
+                BlikiTable,
                 ProfileTable,
                 IdentityTable,
+                GeneratorTable,
             ),
         )
 
@@ -46,30 +52,44 @@ class ExposedAdminRevisionRepositorySpec : FunSpec() {
         beforeTest {
             transaction(db.requireDatabase()) {
                 RevisionTable.deleteAll()
+                EntryTable.deleteAll()
+                BlikiTable.deleteAll()
                 ProfileTable.deleteAll()
                 IdentityTable.deleteAll()
+                GeneratorTable.deleteAll()
             }
         }
 
         context("save") {
+            test("should insert a new revision when the id is provided") {
 
+            }
+
+            test("should insert a new revision when the id is null") {
+
+            }
+
+            test("should update an existing revision when the id already exists") {
+
+            }
         }
 
         context("delete") {
             test("should delete and return the matching revision when it exists") {
+                val identifiers = setupRevisionSimpleFixtures(db.requireDatabase())
                 val revisionId =
                     insertRevision(
                         db = db.requireDatabase(),
                         id = ULID.StatefulMonotonic().nextULID(),
-                        entryId = ULID.StatefulMonotonic().nextULID(),
-                        authorId = ULID.StatefulMonotonic().nextULID(),
+                        entryId = identifiers.entryId!!,
+                        authorId = identifiers.profileId!!,
                     ).id!!
 
                 val result = repository.delete(revisionId).block()
 
-                result shouldBe revisionId
+                result?.id shouldBe revisionId
                 result?.summary shouldBe LocalConstants.Revision.SUMMARY_01
-                result?.diff shouldBe LocalConstants.Revision.DIFF_01
+                result?.diff shouldBe LocalConstants.Revision.DIFF_01.trimIndent()
                 result?.event shouldBe EntryEvent.valueOf(LocalConstants.Revision.EVENT)
             }
 
