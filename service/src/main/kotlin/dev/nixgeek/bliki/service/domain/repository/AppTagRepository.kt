e6a69168e3a8bf3daaf81a27504dfe485b1bb193
Jonas Gorauskas <jgorauskas@gmail.com>
2026-04-08 15:02:11 -0700

    add tag repository interfaces

---
+++ service/src/main/kotlin/dev/nixgeek/bliki/service/domain/repository/AppTagRepository.kt
@@ -0,0 +1,19 @@
+package dev.nixgeek.bliki.service.domain.repository
+
+import dev.nixgeek.bliki.lib.data.ReactorContextAwareRepository
+import dev.nixgeek.bliki.service.domain.model.Tag
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import ulid.ULID
+
+interface AppTagRepository : ReactorContextAwareRepository {
+    fun fetchAll(): Flux<Tag>
+
+    fun fetchById(id: ULID): Mono<Tag>
+
+    fun fetchChildren(id: ULID): Flux<Tag>
+
+    fun fetchParent(id: ULID): Mono<Tag>
+
+    fun fetchDescendants(rootId: ULID): Flux<Tag>
+}
