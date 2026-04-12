cb8c3aca0c79e350f161bd5e1ff828ab71257a31
Jonas Gorauskas <jgorauskas@gmail.com>
2026-04-12 16:17:52 -0700

    add test stubs

---
+++ service/src/test/kotlin/dev/nixgeek/bliki/service/frameworks/data/exposed/repository/ExposedAdminEntryRepositorySpec.kt
@@ -0,0 +1,12 @@
+package dev.nixgeek.bliki.service.frameworks.data.exposed.repository
+
+import io.kotest.core.spec.style.FunSpec
+
+class ExposedAdminEntryRepositorySpec : FunSpec() {
+
+    init {
+        beforeTest {
+
+        }
+    }
+}
